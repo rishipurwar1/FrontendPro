79a93ecab75dc0507da4c78bdf022cafedb069dc
rishipurwar1 <rishipurwar007@gmail.com>
2021-09-13 13:37:38 +0530

    fix resource card size

--- src/components/resources/ResourceCard.js
+++ src/components/resources/ResourceCard.js
@@ -17,8 +17,10 @@ const ResourceCard = ({ resource }) => {
                         <Badge key={tag} name={tag} badgeColor="purple" />
                     ))}
                 </div>
-                <h2 className="font-semibold font-heading text-2xl text-white px-4 pb-1 leading-6 cursor-pointer">{resource.title}</h2>
-                <p className="text-base text-gray-400 px-4 py-2">{resource.description}</p>
+                <div className="card-content">
+                    <h2 className="font-semibold font-heading text-2xl text-white px-4 pb-1 leading-6 cursor-pointer">{resource.title}</h2>
+                    <p className="text-base text-gray-400 px-4 py-2">{resource.description}</p>
+                </div>
                 <a href={resource.link} target="_blank" rel="noopener noreferrer" className="block rounded-t-none mt-4 w-full bg-gradient-to-br from-purple-500 to-indigo-500 text-purple-200 font-semibold font-heading text-lg text-center px-5 py-5" aria-label={resource.title} title={`this is a link to ${resource.title} website`}>View Resource <i className="fas fa-external-link-alt ml-2"></i></a>
             </div>
         </div>

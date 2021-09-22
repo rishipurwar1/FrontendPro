afbe3b55d54017a78589fbd16a5c3eae9e0a32f0
Restyled.io <commits@restyled.io>
2021-09-22 03:48:16 +0000

    Restyled by prettier

--- src/components/challenges/ChallengeDetails.js
+++ src/components/challenges/ChallengeDetails.js
@@ -1,52 +1,69 @@
-import React, {useState} from 'react'
-import useFirestore from '../../hooks/useFirestore'
-import DownloadButton from '../smallComponents/DownloadButton';
-import DropDown from '../smallComponents/DropDown';
-import ChallengeHeader from './ChallengeHeader';
+import React, { useState } from "react";
+import useFirestore from "../../hooks/useFirestore";
+import DownloadButton from "../smallComponents/DownloadButton";
+import DropDown from "../smallComponents/DropDown";
+import ChallengeHeader from "./ChallengeHeader";
 
-const ChallengeDetails =
-    (props) => {
-      const id = props.match.params.id;
-      const {docs} = useFirestore("challenges", id);
-      const [figmaURL, setFigmaURL] = useState(0);
-      const solutionDetails = docs.map(({id, ...r}) => r);
-      if (docs.length === 0)
-        return <p className = "text-white text-center">Loading project...<
-            /p>
-    return (
-        <div className="sm:ml-0 px-5 row-start-2 row-end-3 col-start-2 col-end-3">
-            <ChallengeHeader docs={docs} />
-            <div className = "overflow-hidden relative">
-            <iframe className =
-                 "iframe-embed border-gray-50 w-full -mb-12" src =
-                     {`https://www.figma.com/embed?embed_host=share&url=${
-                         figmaURL === 0 ? (docs[0].figmaURLs.desktop ||
-                                           docs[0].figmaURLs.mobile)
-                                        : docs[0].figmaURLs.mobile}`} title =
-                         {`screen-${figmaURL}`} allowFullScreen>
-            </iframe>
-                {docs[0].figmaURLs.desktop && <DropDown setFigmaURL={(index) => setFigmaURL(index)} />
-    }</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 mt-6">
-                <div className="text-white">
-                    <h2 className="text-2xl font-semibold pb-1 text-purple-500">About the challenge</h2>
-    <p className = "text-gray-300">{docs[0].description}<
-        /p>
-                    <h3 className="text-2xl font-semibold pt-4 pb-2 text-purple-500">Requirements:</h3>
-    <ul className = "text-gray-300 list-disc pl-5"> {docs[0].requirements.map((requirement, index) => (
-                            <li key={index}>{requirement}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="text-white xs:mt-4 sm:mt-0 sm:pl-6">
-                    <h2 className="text-2xl font-semibold pb-1 text-purple-500">What you'll learn?</h2>
-                    <p className="text-gray-300 pb-2">{`${docs[0].learning} So what are you waiting for?`}</p>
-                    <p className="mb-3">Click on the download button to get started. <span role="img" aria-label="hand emoji">👇</span></p>
-                    <DownloadButton color="bg-gradient-to-br from-purple-500 to-indigo-500" challengeDetails={solutionDetails} />
-                </div>
-            </div>
-        </div >
-    )
-}
+const ChallengeDetails = (props) => {
+  const id = props.match.params.id;
+  const { docs } = useFirestore("challenges", id);
+  const [figmaURL, setFigmaURL] = useState(0);
+  const solutionDetails = docs.map(({ id, ...r }) => r);
+  if (docs.length === 0)
+    return <p className="text-white text-center">Loading project...</p>;
+  return (
+    <div className="sm:ml-0 px-5 row-start-2 row-end-3 col-start-2 col-end-3">
+      <ChallengeHeader docs={docs} />
+      <div className="overflow-hidden relative">
+        <iframe
+          className="iframe-embed border-gray-50 w-full -mb-12"
+          src={`https://www.figma.com/embed?embed_host=share&url=${
+            figmaURL === 0
+              ? docs[0].figmaURLs.desktop || docs[0].figmaURLs.mobile
+              : docs[0].figmaURLs.mobile
+          }`}
+          title={`screen-${figmaURL}`}
+          allowFullScreen
+        ></iframe>
+        {docs[0].figmaURLs.desktop && (
+          <DropDown setFigmaURL={(index) => setFigmaURL(index)} />
+        )}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-6">
+        <div className="text-white">
+          <h2 className="text-2xl font-semibold pb-1 text-purple-500">
+            About the challenge
+          </h2>
+          <p className="text-gray-300">{docs[0].description}</p>
+          <h3 className="text-2xl font-semibold pt-4 pb-2 text-purple-500">
+            Requirements:
+          </h3>
+          <ul className="text-gray-300 list-disc pl-5">
+            {" "}
+            {docs[0].requirements.map((requirement, index) => (
+              <li key={index}>{requirement}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="text-white xs:mt-4 sm:mt-0 sm:pl-6">
+          <h2 className="text-2xl font-semibold pb-1 text-purple-500">
+            What you'll learn?
+          </h2>
+          <p className="text-gray-300 pb-2">{`${docs[0].learning} So what are you waiting for?`}</p>
+          <p className="mb-3">
+            Click on the download button to get started.{" "}
+            <span role="img" aria-label="hand emoji">
+              👇
+            </span>
+          </p>
+          <DownloadButton
+            color="bg-gradient-to-br from-purple-500 to-indigo-500"
+            challengeDetails={solutionDetails}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChallengeDetails
+export default ChallengeDetails;

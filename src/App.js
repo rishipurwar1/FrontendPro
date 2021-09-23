5380c0344868584eb8c1e26c3b44e96454c430aa
rishipurwar1 <rishipurwar007@gmail.com>
2021-09-23 12:46:49 +0530

    testing eslint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useEffect } from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import "./App.css"
-import ReactGA from "react-ga"
+import React, { useEffect } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import ReactGA from 'react-ga'
 
 // custom components
-import SideBar from "./components/layouts/SideBar"
-import ChallengesList from "./components/challenges/ChallengesList"
-import Dashboard from "./components/dashboard/Dashboard"
-import Resources from "./components/resources/Resources"
-import Roadmaps from "./components/roadmaps/Roadmaps"
-import ChallengeDetails from "./components/challenges/ChallengeDetails"
-import { AuthProvider } from "./context/AuthContext"
-import SolutionForm from "./components/solutions/SolutionForm"
-import SolutionList from "./components/solutions/SolutionList"
-import SolutionDetails from "./components/solutions/SolutionDetails"
-import SolutionEditForm from "./components/solutions/SolutionEditForm"
-import Navbar from "./components/layouts/Navbar"
-import Footer from "./components/layouts/Footer"
-import MySolutions from "./components/MySolutions/MySolutions"
-import Feedback from "./components/feedback/Feedback"
+import SideBar from './components/layouts/SideBar'
+import ChallengesList from './components/challenges/ChallengesList'
+import Dashboard from './components/dashboard/Dashboard'
+import Resources from './components/resources/Resources'
+import Roadmaps from './components/roadmaps/Roadmaps'
+import ChallengeDetails from './components/challenges/ChallengeDetails'
+import { AuthProvider } from './context/AuthContext'
+import SolutionForm from './components/solutions/SolutionForm'
+import SolutionList from './components/solutions/SolutionList'
+import SolutionDetails from './components/solutions/SolutionDetails'
+import SolutionEditForm from './components/solutions/SolutionEditForm'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+import MySolutions from './components/MySolutions/MySolutions'
+import Feedback from './components/feedback/Feedback'
 
 const App = () => {
   useEffect(() => {
@@ -33,6 +33,7 @@ const App = () => {
         <div className="relative grid md:grid-cols-layout-tablet xl:grid-cols-layout-desktop grid-rows-layout-desktop md:gap-6">
           <Navbar />
           <SideBar />
+          <p>hello</p>
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/challenges" component={ChallengesList} />

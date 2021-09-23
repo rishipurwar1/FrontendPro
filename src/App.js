1653cc2fd43e5abac3f7b5b6a2cf05ddf72ed182
rishipurwar1 <rishipurwar007@gmail.com>
2021-09-23 09:24:24 +0530

    add double to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 import ReactGA from 'react-ga'
 
-// custom components kdjkfdjkj
+// custom components
 import SideBar from './components/layouts/SideBar'
 import ChallengesList from './components/challenges/ChallengesList'
 import Dashboard from './components/dashboard/Dashboard'
@@ -21,36 +21,36 @@ import MySolutions from './components/MySolutions/MySolutions'
 import Feedback from './components/feedback/Feedback'
 
 const App = () => {
-  useEffect(() => {
-    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY)
+    useEffect(() => {
+        ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY)
 
-    // to report page view
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, [])
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <div className="relative grid md:grid-cols-layout-tablet xl:grid-cols-layout-desktop grid-rows-layout-desktop md:gap-6">
-          <Navbar />
-          <SideBar />
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/challenges" component={ChallengesList} />
-            <Route path="/challenge/:id" component={ChallengeDetails} />
-            <Route path="/resources" component={Resources} />
-            <Route path="/roadmaps" component={Roadmaps} />
-            <Route path="/solutions" component={SolutionList} />
-            <Route exact path="/solution/:id" component={SolutionDetails} />
-            <Route path="/solution/:id/edit" component={SolutionEditForm} />
-            <Route path="/submit/:id" component={SolutionForm} />
-            <Route path="/mysolutions" component={MySolutions} />
-          </Switch>
-          <Feedback />
-          <Footer />
-        </div>
-      </AuthProvider>
-    </BrowserRouter>
-  )
+        // to report page view
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    }, [])
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                <div className="relative grid md:grid-cols-layout-tablet xl:grid-cols-layout-desktop grid-rows-layout-desktop md:gap-6">
+                    <Navbar />
+                    <SideBar />
+                    <Switch>
+                        <Route exact path="/" component={Dashboard} />
+                        <Route path="/challenges" component={ChallengesList} />
+                        <Route path="/challenge/:id" component={ChallengeDetails} />
+                        <Route path="/resources" component={Resources} />
+                        <Route path="/roadmaps" component={Roadmaps} />
+                        <Route path="/solutions" component={SolutionList} />
+                        <Route exact path="/solution/:id" component={SolutionDetails} />
+                        <Route path="/solution/:id/edit" component={SolutionEditForm} />
+                        <Route path="/submit/:id" component={SolutionForm} />
+                        <Route path="/mysolutions" component={MySolutions} />
+                    </Switch>
+                    <Feedback />
+                    <Footer />
+                </div>
+            </AuthProvider>
+        </BrowserRouter>
+    )
 }
 
 export default App

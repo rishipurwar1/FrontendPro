ae458cdfccd368e25a6f6b2e1bbf781176fc0699
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-02 22:14:16 +0530

    🎨 improved responsive of ContributerProfile

--- src/components/smallComponents/ContributorProfile.js
+++ src/components/smallComponents/ContributorProfile.js
@@ -3,7 +3,7 @@ import React from "react"
 function ContributorProfile(props) {
   return (
     <div className="flex flex-row items-center mt-3 ">
-      <div className="w-14 mr-4">
+      <div className=" w-12 sm:w-14 mr-4">
         <img
           src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-1-800x800.jpg"
           alt="..."
@@ -11,8 +11,8 @@ function ContributorProfile(props) {
         />
       </div>
       <div className="flex flex-col">
-        <p className="text-lg text-white">Corey Siphron</p>
-        <p className="text-sm text-gray-300">UI/UX Designer</p>
+        <p className="text-base sm:text-lg text-white">Corey Siphron</p>
+        <p className="text-xs sm:text-sm text-gray-300">UI/UX Designer</p>
       </div>
     </div>
   )

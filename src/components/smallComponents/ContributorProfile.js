aafc35374137fd1a012bc088580e28bc18390cf4
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-04 16:34:43 +0530

    ✅ added contributor details.

--- src/components/smallComponents/ContributorProfile.js
+++ src/components/smallComponents/ContributorProfile.js
@@ -1,18 +1,25 @@
 import React from "react"
+import { Image, Placeholder, Transformation } from "cloudinary-react"
 
 function ContributorProfile(props) {
+  const { name, profilePhoto, title } = props.contributor
+
   return (
     <div className="flex flex-row items-center mt-3 ">
-      <div className=" w-12 sm:w-14 mr-4">
-        <img
-          src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-1-800x800.jpg"
-          alt="..."
+      <div className="w-12 sm:w-14 mr-4">
+        <Image
           className="shadow rounded-full max-w-full h-auto border-none"
-        />
+          alt={`Challenge`}
+          cloudName="di5hmgowi"
+          public-id={`${profilePhoto}`}
+        >
+          <Placeholder type="pixelate" />
+          <Transformation crop="fill" />
+        </Image>
       </div>
       <div className="flex flex-col">
-        <p className="text-base sm:text-lg text-white">Corey Siphron</p>
-        <p className="text-xs sm:text-sm text-gray-300">UI/UX Designer</p>
+        <p className="text-base sm:text-lg text-white">{name}</p>
+        <p className="text-xs sm:text-sm text-gray-300">{title}</p>
       </div>
     </div>
   )

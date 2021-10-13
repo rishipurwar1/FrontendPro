e1cba16b6dcb49ca1d4a6f4aa729ae8481a7ab05
Rishi Purwar <57035408+rishipurwar1@users.noreply.github.com>
2021-10-13 10:43:05 +0530

    add proper spacing in profile modal

--- src/components/layouts/SignedInLinks.js
+++ src/components/layouts/SignedInLinks.js
@@ -30,13 +30,13 @@ const SignedInLinks = ({ profile, githubSignOut }) => {
       </button>
       {isOpen && (
         <ul
-          className="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-300 bg-gray-800 rounded-md shadow-md"
+          className="absolute right-0 w-56 py-2 mt-4 space-y-2 text-gray-300 bg-gray-800 rounded-md shadow-md"
           aria-label="sub-menu"
         >
           <li className="rounded transition-all bg-gradient-to-br hover:from-purple-500 hover:to-indigo-500">
             <Link
               to="/mysolutions"
-              className="block px-4 py-1 "
+              className="block px-4 py-2"
               aria-label="my solutions"
               title="this is a link to my solutions page"
             >
@@ -44,7 +44,11 @@ const SignedInLinks = ({ profile, githubSignOut }) => {
             </Link>
           </li>
           <li className="rounded transition-all bg-gradient-to-br hover:from-purple-500 hover:to-indigo-500">
-            <button disabled={loading} onClick={logout} className="block px-4 py-1">
+            <button
+              disabled={loading}
+              onClick={logout}
+              className="w-full text-left block px-4 py-2"
+            >
               <i className="fas fa-sign-out-alt mr-2"></i>Logout
             </button>
           </li>

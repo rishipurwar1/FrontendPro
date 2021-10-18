80b0d89e5906540e7c2f2caa87d32e53d01e74c6
Harsh Pareek <harshpareek@Harshs-MacBook-Air.local>
2021-10-18 15:34:06 +0530

    hover effect on buttons and cards

--- src/components/dashboard/Card.js
+++ src/components/dashboard/Card.js
@@ -3,9 +3,9 @@ import React from "react"
 const Card = ({ name, info, image, gradient, color, lastClass }) => {
   return (
     <div
-      className={`${gradient} rounded-xl shadow-2xl text-center px-5 py-8 max-w-sm h-96 ${
-        lastClass ? "sm:last:col-span-2 lg:last:col-span-1" : ""
-      }`}
+      className={`${gradient} rounded-xl shadow-2xl text-center px-5 py-8 max-w-sm h-96 opacity-80
+      hover:shadow-2xl hover:opacity-100 
+      ${lastClass ? "sm:last:col-span-2 lg:last:col-span-1" : ""}`}
     >
       <i className={`${image} text-8xl ${color} pb-3`}></i>
       <p className="py-3 text-2xl text-white font-bold font-heading">{name}</p>
--- src/components/smallComponents/Button.js
+++ src/components/smallComponents/Button.js
@@ -5,7 +5,9 @@ const Button = ({ handleClick, bgColor, disabled, name, logo }) => {
     <button
       disabled={disabled}
       onClick={handleClick}
-      className={`cursor-pointer block w-56 ${bgColor} text-white text-base p-4 font-heading font-semibold shadow-md rounded-xl focus:outline-none`}
+      className={` block w-10000 ${bgColor} text-gray-300
+      hover:shadow-2xl hover:translate-x hover:text-white
+      text-white text-base p-4 font-heading font-semibold shadow-md rounded-xl focus:outline-none`}
     >
       {name}
       {logo ? <i className={logo}></i> : null}

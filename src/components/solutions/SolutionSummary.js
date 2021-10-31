1afff44785f6f235bcac2ffff02efac6a036181c
tausifaman222 <tausifaman222@gmail.com>
2021-10-31 14:39:17 +0530

    feat:Add hover effects on cards, closes:#132

--- src/components/solutions/SolutionSummary.js
+++ src/components/solutions/SolutionSummary.js
@@ -5,7 +5,7 @@ import { Image, Placeholder } from "cloudinary-react"
 
 const SolutionSummary = ({ solution }) => {
   return (
-    <div className="max-w-sm h-auto rounded-md overflow-hidden shadow-2xl">
+    <div className="max-w-sm h-auto rounded-md overflow-hidden shadow-2xl transform transition duration-300 ease-in-out hover:shadow-primary hover:scale-105">
       <Image
         cloudName="di5hmgowi"
         loading="lazy"

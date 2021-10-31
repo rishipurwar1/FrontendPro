07e7d0f1fefb8b12714e2aa6725ac18b24447a43
tausifaman222 <tausifaman222@gmail.com>
2021-10-31 14:39:17 +0530

    feat:Add hover effects on cards, closes:#132

--- tailwind-config.js
+++ tailwind-config.js
@@ -35,6 +35,10 @@ module.exports = {
       gridTemplateRows: {
         "layout-desktop": "80px 1fr 80px",
       },
+      boxShadow: {
+        primary:
+          "0px 15px 25px rgba(148, 163, 184, 0.15), 0px 5px 10px rgba(148, 163, 184, 0.05)",
+      },
     },
   },
   variants: {

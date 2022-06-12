7cba8aa736a1bd2953e731928bc6248d6385736d
Rishi Purwar <57035408+rishipurwar1@users.noreply.github.com>
2022-06-12 13:25:17 +0530

    Fix: delete modal

--- src/components/reusable/ConfirmationModal.js
+++ src/components/reusable/ConfirmationModal.js
@@ -15,28 +15,28 @@ const ConfirmationModal = ({ setModal, id }) => {
 
   return (
     <div
-      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-gray-200 bg-opacity-10 pl-56"
+      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-0 outline-none focus:outline-none bg-gray-200 bg-opacity-10 pl-56"
       onClick={() => setModal(false)}
     >
-      <div className="shadow-lg rounded-2xl p-4 z-50 bg-gray-800 w-64 m-auto">
+      <div className="shadow-lg rounded-2xl p-4 z-50 bg-gray-800 w-auto max-w-sm m-auto">
         <div className="w-full h-full text-center">
           <div className="flex h-full flex-col justify-between">
-            <i className="fas fa-trash-alt"></i>
-            <p className="text-gray-200 text-xl font-bold mt-4">Remove solution</p>
-            <p className="text-gray-200 text-xs py-2 px-6">
+            <i className="fas fa-trash-alt text-gray-300"></i>
+            <p className="text-gray-300 text-xl font-bold mt-4">Remove solution</p>
+            <p className="text-gray-400 text-lg font-heading leading-relaxed text-center py-2 px-6">
               Are you sure you want to delete this solution ?
             </p>
             <div className="flex items-center justify-between gap-4 w-full mt-8">
               <button
                 type="button"
-                className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
+                className="py-2 px-4 bg-purple-800 transition-colors duration-200 bg-gradient-to-br hover:from-purple-500 hover:to-indigo-500 text-white w-full text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
                 onClick={handleDelete}
               >
                 Delete
               </button>
               <button
                 type="button"
-                className="py-2 px-4  bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-indigo-500  w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
+                className="py-2 px-4 bg-white hover:bg-gray-200 text-purple-800 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
                 onClick={() => setModal(false)}
               >
                 Cancel

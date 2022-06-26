e20e8c19d3456f74e7b6bba8e4f6428f11f3c17f
rishipeerlist <rishi@peerlist.io>
2022-06-26 14:10:53 +0530

    Fix: revamp challenge details page and add faq section

---
+++ src/components/reusable/Accordion.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import { faqs } from "../../utils/data"
+
+import AccordionItem from "./AccordionItem"
+
+const Accordion = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center mt-16">
+        <div className="w-full mx-auto md:px-4 mb-2">
+          <h2 className="text-purple-500 text-3xl md:text-4xl font-heading font-bold tracking-wide text-center lg:my-8">
+            Frequently Asked Questions
+          </h2>
+          <section className="shadow-md overflow-hidden rounded-lg mt-6 sm:shadow-lg lg:mt-10 lg:mx-auto lg:max-w-4xl">
+            {faqs.map((faq, index) => (
+              <AccordionItem key={index} faq={faq} />
+            ))}
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Accordion

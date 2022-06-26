d5007dd47d149c72714e0514ff362db15183743f
Rishi Purwar <rishipurwar007@gmail.com>
2022-06-26 15:21:20 +0530

    Revamp challenge details UI and add an accordion

---
+++ src/utils/data.js
@@ -0,0 +1,17 @@
+export const faqs = [
+  {
+    question: "Can I use libraries/frameworks on these projects?",
+    answer:
+      "Yes, absolutely! We're providing the vanilla HTML, CSS and JavaScript files, but you can use whatever tools and frameworks you'd like. We can't wait to see what you come up with.",
+  },
+  {
+    question: "What is the best way to get help when I'm stuck?",
+    answer:
+      "We have a great community of developers that are happy to help out with your project. You can join our Discord server and chat with other developers or post questions in our #general channel.",
+  },
+  {
+    question: "Can I add this project in my portfolio?",
+    answer:
+      "Absolutely! These projects are perfect for your portfolio, and they're a great way to show off your web development skills. But, be honest about the work that you did.",
+  },
+]

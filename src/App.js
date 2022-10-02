f2185f304d448e315c5b4bd35c795eb030b429f2
rishipurwar1 <rishipurwar007@gmail.com>
2022-10-02 10:16:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 
 // loader
 import rocketLoader from "./assets/animated_illustrations/loader.json"
-import ChallengeDetails from "./components/challenges/ChallengeDetails"
-// lazy loading components
-import ChallengesList from "./components/challenges/ChallengesList"
-import Dashboard from "./components/dashboard/Dashboard"
 import Footer from "./components/layouts/Footer"
 import Navbar from "./components/layouts/Navbar"
 // custom components
 import SideBar from "./components/layouts/SideBar"
-import MySolutions from "./components/MySolutions/MySolutions"
-import Resources from "./components/resources/Resources"
 import LottieAnimation from "./components/reusable/LottieAnimation"
 import ScrollToTop from "./components/reusable/ScrollToTop"
-import Roadmaps from "./components/roadmaps/Roadmaps"
-import SolutionDetails from "./components/solutions/SolutionDetails"
-import SolutionEditForm from "./components/solutions/SolutionEditForm"
-import SolutionForm from "./components/solutions/SolutionForm"
-import SolutionList from "./components/solutions/SolutionList"
 import { useAuthContext } from "./hooks/useAuthContext"
 import useGaTracker from "./hooks/useGaTracker"
 
 import "./App.css"
+
+// lazy loading components
+const Dashboard = React.lazy(() => import("./components/dashboard/Dashboard"))
+const ChallengesList = React.lazy(() => import("./components/challenges/ChallengesList"))
+const ChallengeDetails = React.lazy(() =>
+  import("./components/challenges/ChallengeDetails")
+)
+const Resources = React.lazy(() => import("./components/resources/Resources"))
+const Roadmaps = React.lazy(() => import("./components/roadmaps/Roadmaps"))
+const SolutionList = React.lazy(() => import("./components/solutions/SolutionList"))
+const SolutionDetails = React.lazy(() => import("./components/solutions/SolutionDetails"))
+const SolutionForm = React.lazy(() => import("./components/solutions/SolutionForm"))
+const SolutionEditForm = React.lazy(() =>
+  import("./components/solutions/SolutionEditForm")
+)
+const MySolutions = React.lazy(() => import("./components/MySolutions/MySolutions"))
 
 const App = () => {
   const { authIsReady, user } = useAuthContext()
@@ -32,30 +37,38 @@ const App = () => {
     <div className="relative grid min-h-screen md:grid-cols-layout-tablet xl:grid-cols-layout-desktop grid-rows-layout-desktop md:gap-6 xxl:max-w-screen-xxl mx-auto">
       <Navbar />
       <SideBar />
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/challenges" element={<ChallengesList />} />
-          <Route path="/challenge/:id" element={<ChallengeDetails />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/roadmaps" element={<Roadmaps />} />
-          <Route path="/solutions" element={<SolutionList />} />
-          <Route path="/solution/:id" element={<SolutionDetails />} />
-          <Route
-            path="/submit/:id"
-            element={user ? <SolutionForm /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/solution/:id/edit"
-            element={user ? <SolutionEditForm /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/mysolutions"
-            element={user ? <MySolutions /> : <Navigate to="/" />}
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </ScrollToTop>
+      <Suspense
+        fallback={
+          <div className="sm:ml-0 pr-5 py-52 row-start-2 row-end-3 col-start-1 md:col-start-2 col-end-3 place-self-center">
+            <LottieAnimation animationDataFile={rocketLoader} height={100} width={100} />
+          </div>
+        }
+      >
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/challenges" element={<ChallengesList />} />
+            <Route path="/challenge/:id" element={<ChallengeDetails />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/roadmaps" element={<Roadmaps />} />
+            <Route path="/solutions" element={<SolutionList />} />
+            <Route path="/solution/:id" element={<SolutionDetails />} />
+            <Route
+              path="/submit/:id"
+              element={user ? <SolutionForm /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/solution/:id/edit"
+              element={user ? <SolutionEditForm /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/mysolutions"
+              element={user ? <MySolutions /> : <Navigate to="/" />}
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </ScrollToTop>
+      </Suspense>
       <Footer />
     </div>
   ) : (

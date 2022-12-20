372bd7a93343bba776d15bc02972e85557db0296
rishipurwar1 <rishipurwar007@gmail.com>
2022-12-20 13:26:35 +0530

    Update solution form ui

--- src/pages/SolutionForm.js
+++ src/pages/SolutionForm.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { useNavigate, useParams } from "react-router-dom"
 
 import mainImg from "../assets/animated_illustrations/solution_animation.json"
 import Hero from "../components/homepage/Hero"
 import Modal from "../components/reusable/Modal"
-import { analytics, logEvent } from "../firebase/config"
 import { useFirestore } from "../hooks/useFirestore"
 
 const SolutionForm = () => {
@@ -36,10 +35,6 @@ const SolutionForm = () => {
     }
   }
 
-  useEffect(() => {
-    logEvent(analytics, "solution_submit_page_visited")
-  }, [])
-
   return (
     <div className="px-5 row-start-2 row-end-3 col-start-2 col-end-3">
       <Hero
@@ -51,80 +46,75 @@ const SolutionForm = () => {
         route="/solutions"
         lottie
       />
-      <div className="w-full mx-auto mt-8">
-        <form
-          className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4"
-          onSubmit={handleSubmit(onSubmit)}
-        >
+      <h2 className="text-5xl text-center text-white font-extrabold">Submit Solution</h2>
+      <div className="p-4 rounded-lg shadow bg-gray-800 sm:p-5 mt-8">
+        <form onSubmit={handleSubmit(onSubmit)}>
           {/* challenge title */}
           <div className="mb-4">
-            <label
-              className="block text-gray-400 text-base font-bold mb-2"
-              htmlFor="title"
-            >
+            <label className="block mb-2 text-sm font-medium text-white" htmlFor="title">
               Challenge Title
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-1 focus:ring-gray-500 focus:outline-none focus:border-gray-500"
               id="title"
               type="text"
               placeholder="Enter Title"
               {...register("title", { required: true })}
             />
             {errors.title?.type === "required" && (
-              <span className="text-red-500 text-xs">Title is required</span>
+              <small className="text-red-500 text-xs">Title is required</small>
             )}
           </div>
           {/* challenge github url */}
-          <div className="mb-6">
+          <div className="mb-4">
             <label
-              className="block text-gray-400 text-base  font-bold mb-2"
+              className="block mb-2 text-sm font-medium text-white"
               htmlFor="githubUrl"
             >
               Github Repository URL
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-1 focus:ring-gray-500 focus:outline-none focus:border-gray-500"
               id="githubUrl"
               type="text"
               placeholder="Github Repository URL"
               {...register("githubUrl", { required: true })}
             />
             {errors.githubUrl?.type === "required" && (
-              <span className="text-red-500 text-xs">
+              <small className="text-red-500 text-xs">
                 Github Repository URL is required
-              </span>
+              </small>
             )}
           </div>
           {/* live website url */}
-          <div className="mb-6">
+          <div className="mb-4">
             <label
-              className="block text-gray-400 text-base font-bold mb-2"
+              className="block mb-2 text-sm font-medium text-white"
               htmlFor="liveWebsiteUrl"
             >
               Live Website URL
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-1 focus:ring-gray-500 focus:outline-none focus:border-gray-500"
               id="liveWebsiteUrl"
               type="text"
               placeholder="Live Website URL"
               {...register("liveWebsiteUrl", { required: true })}
             />
             {errors.liveWebsiteUrl?.type === "required" && (
-              <span className="text-red-500 text-xs">Live Website URL is required</span>
+              <small className="text-red-500 text-xs">Live Website URL is required</small>
             )}
           </div>
           {/* feedback */}
-          <div className="mb-6">
+          <div className="mb-4">
             <label
-              className="block text-gray-400 text-base font-bold mb-2"
+              className="block mb-2 text-sm font-medium text-white"
               htmlFor="feedback"
             >
               Ask for feedback
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-1 focus:ring-gray-500 focus:outline-none focus:border-gray-500"
               id="feedback"
               type="text"
               placeholder="Ask for feedback from the community."
@@ -132,45 +122,43 @@ const SolutionForm = () => {
             />
           </div>
           {/* submit button */}
-          <div>
-            <button className="inline-flex items-center justify-center bg-gradient-to-br from-purple-500 to-indigo-500 text-purple-200 font-bold text-base py-3 px-4 rounded focus:outline-none focus:shadow-outline w-full">
-              {response.isPending ? (
-                <>
-                  <svg
-                    className="animate-spin mr-2 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                  Submitting...
-                </>
-              ) : (
-                "Submit Solution"
-              )}
-            </button>
-            {state && (
-              <Modal
-                setShowModal={setState}
-                auth
-                title="Thank You for submitting your solution."
-                emoji="🙏"
-              />
+          <button className="text-white inline-flex items-center focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors bg-indigo-600 hover:bg-indigo-700 focus:ring-blue-800">
+            {response.isPending ? (
+              <>
+                <svg
+                  className="animate-spin mr-2 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Submitting...
+              </>
+            ) : (
+              "Submit Solution"
             )}
-          </div>
+          </button>
+          {state && (
+            <Modal
+              setShowModal={setState}
+              auth
+              title="Thank You for submitting your solution."
+              emoji="🙏"
+            />
+          )}
         </form>
       </div>
     </div>

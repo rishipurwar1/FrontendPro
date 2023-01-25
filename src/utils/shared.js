2492a4f0b1502e11d7d87bf001850fe5559b28d9
rishipurwar1 <rishipurwar007@gmail.com>
2023-01-25 20:38:00 +0530

    Add getLanguageOfFile function to utils

--- src/utils/shared.js
+++ src/utils/shared.js
@@ -29,3 +29,25 @@ export function debounce(callback, delay) {
     }, delay)
   }
 }
+
+export const getLanguageOfFile = (filePath) => {
+  const extensionDotIndex = filePath.lastIndexOf(".")
+  const extension = filePath.slice(extensionDotIndex + 1)
+
+  switch (extension) {
+    case "js":
+    case "jsx":
+    case "ts":
+    case "tsx":
+      return "javascript"
+    case "vue":
+    case "html":
+      return "html"
+    case "css":
+    case "scss":
+    case "less":
+      return "css"
+    default:
+      return "javascript"
+  }
+}

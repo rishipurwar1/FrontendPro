52c5e31710c8f8c1530efda434fbdeeabddc93e9
rishipurwar1 <rishipurwar007@gmail.com>
2023-03-23 07:27:29 +0530

    Add headers for custom preview domain

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "avatars.githubusercontent.com"],
   },
+  async headers() {
+    const headers = []
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      })
+    }
+    return headers
+  },
 }
 
 module.exports = nextConfig

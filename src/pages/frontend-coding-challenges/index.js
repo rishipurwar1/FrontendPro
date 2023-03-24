66fc0f185c324963bdcf4c92c4593d3b372bc6fb
rishipurwar1 <rishipurwar007@gmail.com>
2023-03-24 22:55:04 +0530

    Add meta tags + sitemap

--- src/pages/frontend-coding-challenges/index.js
+++ src/pages/frontend-coding-challenges/index.js
@@ -1,24 +1,30 @@
 import Header from "../../components/reusable/Header"
 import Card from "../../components/reusable/Card"
 import { getDocuments } from "../../firebase/firestore"
+import Head from "next/head"
 
 const FrontendCodingChallenges = ({ challenges }) => {
   return (
-    <main>
-      <Header
-        title="Frontend Coding Challenges"
-        description="Take your Frontend coding skills to the next level with our curated collection
+    <>
+      <Head>
+        <title>FrontendPro - Frontend Coding Challenges</title>
+      </Head>
+      <main>
+        <Header
+          title="Frontend Coding Challenges"
+          description="Take your Frontend coding skills to the next level with our curated collection
           of real-world frontend coding challenges. 🚀"
-        gradientClasses="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
-      />
-      <section className="rounded-lg bg-gray-900 border border-gray-700 p-6">
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-          {challenges?.map((challenge) => {
-            return <Card key={challenge.id} card={challenge} isChallenge />
-          })}
-        </div>
-      </section>
-    </main>
+          gradientClasses="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
+        />
+        <section className="rounded-lg bg-gray-900 border border-gray-700 p-6">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+            {challenges?.map((challenge) => {
+              return <Card key={challenge.id} card={challenge} isChallenge />
+            })}
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 

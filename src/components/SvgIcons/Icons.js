b84f69797b40bb4bf14db146f1050d64a54c0ac1
rishipurwar1 <rishipurwar007@gmail.com>
2023-03-25 11:22:53 +0530

    Update quote icon attribute

--- src/components/SvgIcons/Icons.js
+++ src/components/SvgIcons/Icons.js
@@ -595,15 +595,15 @@ const Icons = {
   Quote: ({ size = 24, ...props }) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-quote"
+      className="icon icon-tabler icon-tabler-quote"
       width={size}
       height={size}
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
